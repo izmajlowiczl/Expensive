c80a4052570ad307b521a3a81f19d1a95f048f7f
izmajlowiczl <izmajlowiczl@gmail.com>
2016-07-01 00:12:35 +0200

    Added BudgetsActivity from empty-activity template.

---
+++ app/src/main/java/pl/expensive/BudgetsActivity.java
@@ -0,0 +1,13 @@
+package pl.expensive;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BudgetsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_budgets);
+    }
+}
