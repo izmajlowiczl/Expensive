88f4045151b3d7d00b74a379e17f594788db5b1e
izmajlowiczl <izmajlowiczl@gmail.com>
2016-07-19 23:45:44 +0200

    Added test dependencies
    New dependencies mockito(v2.0.87-beta), truth(v0.28), hamcrest(v1.3),
    guava(v1.19)-> needed only for truth
    
    New dependencies for Reactive programming

--- app/src/test/java/pl/expensive/ExampleUnitTest.java
+++ app/src/test/java/pl/expensive/ExampleUnitTest.java
@@ -2,14 +2,11 @@ package pl.expensive;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static com.google.common.truth.Truth.assertThat;
 
-/**
- * To work on unit tests, switch the Test Artifact in the Build Variants view.
- */
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+        assertThat(2+2).isEqualTo(4);
     }
 }
