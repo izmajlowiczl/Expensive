eceabecf462956627ed84c3aa92b4f8cdf770574
lukasz izmajlowicz <izmajlowiczl@gmail.com>
2017-01-01 13:53:29 +0100

    Database class get cleaned
    Removed old comments
    TODO: Check why pragma for foreign keys is enabled only on readonly database.

--- app/src/main/java/pl/expensive/db/Database.java
+++ app/src/main/java/pl/expensive/db/Database.java
@@ -21,7 +21,6 @@ public class Database extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL("CREATE TABLE tbl_wallet (uuid TEXT, name TEXT NOT NULL UNIQUE, PRIMARY KEY(uuid));");
-//        sqLiteDatabase.execSQL("CREATE TABLE tbl_transaction (uuid TEXT, name TEXT NOT NULL, value TEXT NOT NULL, currency TEXT NOT NULL, start INTEGER NOT NULL, wallet TEXT, PRIMARY KEY(uuid), FOREIGN KEY(wallet) REFERENCES tbl_wallet(uuid) ON DELETE CASCADE);");
     }
 
     @Override
@@ -33,42 +32,9 @@ public class Database extends SQLiteOpenHelper {
     public void onOpen(SQLiteDatabase db) {
         super.onOpen(db);
         // Enable Foreign keys to be able to delete cascade
+        // Fixme: 01.01.2017 Why is it for read only db?
         if (db.isReadOnly()) {
             db.execSQL("PRAGMA foreign_keys=ON;");
         }
     }
-
-//    fun insertTransaction(transaction: Transaction) {
-//        try {
-//            writableDatabase.insertOrThrow("tbl_transaction", null, transaction.toContentValues())
-//        } catch (sqlEx: SQLException) {
-//            if (sqlEx is SQLiteConstraintException) {
-//                throw IllegalStateException("Inserting transaction [" + transaction + "] for not existing wallet [" + transaction.wallet.toString() + "]")
-//            }
-//        }
-//    }
-//
-//    fun listTransactions(wallet:UUID): Collection<Transaction>
-//
-//    {
-//        val cursor = readableDatabase.query("tbl_transaction", null, "wallet=?", arrayOf(wallet.toString()), null, null, null)
-//        val transactions = ArrayList<Transaction>()
-//        while (cursor.moveToNext()) {
-//            transactions.add(fromCursor(cursor, wallet))
-//        }
-//        cursor.close()
-//        return transactions
-//    }
-//
-//    private fun fromCursor(cursor:Cursor, wallet: UUID): Transaction {
-//        return Transaction(
-//                UUID.fromString(cursor.getString(cursor.getColumnIndexOrThrow("uuid"))),
-//                cursor.getString(cursor.getColumnIndexOrThrow("name")),
-//                Money(
-//                        BigDecimal(cursor.getString(cursor.getColumnIndexOrThrow("value"))),
-//                        Locale.getDefault() // Fixme: Restore locale from stored value
-//                ),
-//                Date(cursor.getLong(cursor.getColumnIndexOrThrow("start"))),
-//                wallet)
-//    }
 }
