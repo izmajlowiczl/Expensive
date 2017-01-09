303e950e8fcb76a715c40b131f64ac3909d1c2e6
lukasz izmajlowicz <izmajlowiczl@gmail.com>
2017-01-09 09:02:17 +0100

    Added LeakCanary for automatic memory leak finding.
    Check https://github.com/square/leakcanary for more info.
    LeakCanary.install(app) sets RefWatcher for acitvities automatically. There is nothing else so far to watch at, so no need for any extra RefWatche.

--- app/src/main/java/pl/expensive/App.java
+++ app/src/main/java/pl/expensive/App.java
@@ -2,12 +2,19 @@ package pl.expensive;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+
 import pl.expensive.storage.StorageModule;
 
 public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return;
+        }
+        LeakCanary.install(this);
 
         AppComponent appComponent = DaggerAppComponent.builder()
                 .storageModule(new StorageModule(this))
