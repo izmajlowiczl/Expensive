83482a72ed16f0679e2cbf87b749437e72bb88eb
lukasz izmajlowicz <izmajlowiczl@gmail.com>
2017-02-01 10:34:41 +0100

    Added kotlin-android-extensions
    Added to avoid calls to findViewById. WalletsActivity was migrated already to use it.

---
+++ app/src/main/java/pl/expensive/WalletsActivity.kt
@@ -0,0 +1,27 @@
+package pl.expensive
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_wallets.*
+
+class WalletsActivity : AppCompatActivity() {
+    private lateinit var fetchWallets: DisplayWallets
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wallets)
+
+        Injector.app().inject(this)
+
+        fetchWallets = Injector.app().displayWallets()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        showWallets()
+    }
+
+    private fun showWallets() {
+        fetchWallets.runFor(wallets)
+    }
+}
