1161a0bc28f949a3689b0454d94eb18bc94a111c
lukasz izmajlowicz <izmajlowiczl@gmail.com>
2017-02-13 11:34:33 +0100

    Formatting time in transaction to display day and time

--- app/src/main/java/pl/expensive/transaction/TransactionViewHolder.kt
+++ app/src/main/java/pl/expensive/transaction/TransactionViewHolder.kt
@@ -1,6 +1,8 @@
 package pl.expensive.transaction
 
 import android.support.v7.widget.RecyclerView
+import android.text.SpannableString
+import android.text.style.RelativeSizeSpan
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
@@ -11,22 +13,33 @@ import java.text.DecimalFormat
 import java.util.*
 
 class TransactionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    private val timeFormat = DateTimeFormatter.ofPattern("HH:mm")
-
-    fun update(viewModel: Transaction) {
-        with(itemView) {
-            transaction_amount.text = viewModel.formatValue()
-            transaction_time.text = viewModel.toLocalDateTime().toLocalTime().format(timeFormat)
-            if (viewModel.description.isNullOrBlank()) {
-                transaction_desc.visibility = GONE
-            } else {
-                transaction_desc.visibility = VISIBLE
-                transaction_desc.text = viewModel.description
-            }
+    fun update(viewModel: Transaction) = with(itemView) {
+        transaction_amount.text = viewModel.formatAmount()
+        transaction_time.text = viewModel.formatTime()
+        if (viewModel.description.isNullOrBlank()) {
+            transaction_desc.visibility = GONE
+        } else {
+            transaction_desc.visibility = VISIBLE
+            transaction_desc.text = viewModel.description
         }
     }
 
-    private fun Transaction.formatValue(locale: Locale = Locale.getDefault()): String {
+    private val timeFormat = DateTimeFormatter.ofPattern("HH:mm")
+    private val dateTimeFormat = DateTimeFormatter.ofPattern("MMM d")
+
+    /**
+     * Formats transaction time to show date (eg Jan 12) in normal text size.
+     * Time (22:12) in 70% of date text size in new line
+     */
+    private fun Transaction.formatTime(): CharSequence {
+        val localDateTime = toLocalDateTime()
+        val format = localDateTime.format(dateTimeFormat)
+        val span = SpannableString("$format \n ${localDateTime.format(timeFormat)}")
+        span.setSpan(RelativeSizeSpan(.7f), span.length - format.length, span.length, 0)
+        return span
+    }
+
+    private fun Transaction.formatAmount(locale: Locale = Locale.getDefault()): String {
         val numberFormat = DecimalFormat.getInstance(locale) as DecimalFormat
         numberFormat.applyPattern(currency.format)
         return numberFormat.format(amount)
